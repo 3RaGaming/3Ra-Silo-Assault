693fd5ea13f974cb678157a29e862a5f6bbe2ea0
Ps7cho <chocolatetthunder@gmail.com>
2017-02-04 16:07:48 -0600

    force a nill value

--- control.lua
+++ control.lua
@@ -241,12 +241,14 @@ Event.register(defines.events.on_tick, function(event)
 			for force_name, votes in pairs(global.surrender_votes) do
 				if votes.in_progress and game.tick >= votes.vote_start_time + global.surrender_voting_period * 3600 then
 					local force = game.forces[force_name]
-					force.print({"voting-period-timed-out"})
-					votes.in_progress = false
-					for i,p in pairs(force.players) do
-						p.gui.top.surrender_button.style.font_color = colors.red
-						if p.gui.left.surrender_dialog then open_surrender_window(p) end
-					end
+					if force and force.valid then
+						force.print({"voting-period-timed-out"})
+						votes.in_progress = false
+						for i,p in pairs(force.players) do
+							p.gui.top.surrender_button.style.font_color = colors.red
+							if p.gui.left.surrender_dialog then open_surrender_window(p) end
+						end
+					end	
 				end
 			end
 		end
