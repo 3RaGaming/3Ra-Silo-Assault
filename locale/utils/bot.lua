f1e04a8b84062f0870f150faa9de885dc81c6e87
Zachary Sistrunk <computerboyzack@gmail.com>
2017-05-19 20:33:55 -0500

    Updating bot to latest 0.15 code

--- locale/utils/bot.lua
+++ locale/utils/bot.lua
@@ -1,44 +1,35 @@
 Event.register(defines.events.on_player_died, function (event)
-	local player = event.player_index
-	if game.players[player].name ~= nil then
-		print("PLAYER$die," .. player .. "," .. game.players[player].name .. "," .. game.players[player].force.name)
+	local player = game.players[event.player_index]
+	if player ~= nil then
+		print("PLAYER$die," .. player.index .. "," .. player.name .. "," .. player.force.name)
 	end
 end)
 
 Event.register(defines.events.on_player_respawned, function (event)
-	local player = event.player_index
-	if game.players[player].name ~= nil then
-		print("PLAYER$respawn," .. player .. "," .. game.players[player].name .. "," .. game.players[player].force.name)
+	local player = game.players[event.player_index]
+	if player ~= nil then
+		print("PLAYER$respawn," .. player.index .. "," .. player.name .. "," .. player.force.name)
 	end
 end)
 
 Event.register(defines.events.on_player_joined_game, function (event)
-	local player = event.player_index
-	if game.players[player].name ~= nil then
-		print("PLAYER$join," .. player .. "," .. game.players[player].name .. "," .. game.players[player].force.name)
+	local player = game.players[event.player_index]
+	if player ~= nil then
+		print("PLAYER$join," .. player.index .. "," .. player.name .. "," .. player.force.name)
 	end
 end)
 
 Event.register(defines.events.on_player_left_game, function (event)
-	local player = event.player_index
-	if game.players[player].name ~= nil then
-		print("PLAYER$leave," .. player .. "," .. game.players[player].name .. "," .. game.players[player].force.name)
+	local player = game.players[event.player_index]
+	if player ~= nil then
+		print("PLAYER$leave," .. player.index .. "," .. player.name .. "," .. player.force.name)
 	end
 end)
 
-function heartbeat()
-	--Do nothing, this is just so managepgm can call something as a heartbeat without any errors occurring
-end
-
-function playerQuery()
-	if #game.connected_players == 0 then
-		print("output$pquery$none")
-	else
-		local response = "output&pquery$"
-		for _,player in pairs(game.connected_players) do
-			local playerdata = player.name .. "-" .. player.force.name
-			response = response .. playerdata .. ","
-		end
-		print(response:sub(1,#str-1))
+Event.register(defines.events.on_player_changed_force, function (event)
+	local player = game.players[event.player_index]
+	if (player.force.name == "Admins") then return end
+	if player ~= nil then
+		print("PLAYER$force," .. player.index .. "," .. player.name .. "," .. player.force.name)
 	end
-end
+end)
